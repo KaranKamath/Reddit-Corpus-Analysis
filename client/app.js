1e25b1bac7cdfe2bee4835ce3e63fb7daa632b5b
Karan Kamath <karankamath26@gmail.com>
2016-01-24 07:57:00 +0800

    Proto for list does something

--- client/app.js
+++ client/app.js
@@ -2,6 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var QueryForm = require('./form').form;
 var WordCloud = require('./d3Comp').cloud;
+var TopicList = require('./topiclist').topiclist;
 
 var injectTapEventPlugin = require('react-tap-event-plugin');
 
@@ -24,7 +25,12 @@ defaults = {
         { text: "more", size: 10 + Math.random() * 90 },
         { text: "words", size: 10 + Math.random() * 90 },
         { text: "kappa", size: 10 + Math.random() * 90 },
-    ],              
+    ],
+    topics: [
+        { words:[ 'bernie', 'democratic', 'ellen', 'second'], 'rating': { 'happy':44, 'sad': 433 }},
+        { words:[ 'hillary', 'democratic', 'benghazi', 'email'], 'rating': { 'happy':56, 'sad': 43 }},    
+        { words:[ 'trump', 'guns', 'cruz', 'canada', 'republican'], 'rating': { 'happy':74, 'sad': 298 }},
+    ],   
 };
 
 var Main = React.createClass({
@@ -37,7 +43,10 @@ var Main = React.createClass({
     return (
     <div style={{ fontFamily: "'Roboto', san-serif",}}>
       <QueryForm month={defaults.month} year={defaults.year} subreddit={defaults.sub} onSubmit={this.handleSubmit}/>
-      <WordCloud wordmap={defaults.wordmap}/>
+      <div style={{ display: 'flex'}}>
+        <WordCloud style={{ flexGrow: 1 }} wordmap={defaults.wordmap}/>
+        <TopicList topics={defaults.topics} style={{ flexGrow: 3}}></TopicList>
+      </div>
     </div>
     );
   }
