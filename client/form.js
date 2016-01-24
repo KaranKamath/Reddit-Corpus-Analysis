f8167aa7008fc6afa5b5453ee221b3d175b3af0e
Karan Kamath <karankamath26@gmail.com>
2016-01-24 11:57:30 +0800

    Fix annoying bug

--- client/form.js
+++ client/form.js
@@ -23,8 +23,8 @@ var QueryForm = React.createClass({
 
   handleMonthChange: function(evt) {
     this.setState({month:evt.target.innerText});
-    console.log(evt);
-    console.log(this.state);
+    //console.log(evt);
+    //console.log(this.state);
   },
 
   handleSubredditChange: function(evt) {
@@ -44,30 +44,30 @@ var QueryForm = React.createClass({
       <form style={styles.form}>
           <span style={styles.field}>
             <DropDownMenu value={this.state.subreddit} onChange={this.handleSubredditChange}>
-              <MenuItem value={1} primaryText="news"/>
-              <MenuItem value={2} primaryText="politics"/>
+              <MenuItem value="/r/news" primaryText="news"/>
+              <MenuItem value="/r/politics" primaryText="politics"/>
             </DropDownMenu>
           </span>  
           <span style={styles.field}>
               <SelectField value={this.state.month} onChange={this.handleMonthChange}>
-                <MenuItem value={1} primaryText="January"/>
-                <MenuItem value={2} primaryText="February"/>
-                <MenuItem value={3} primaryText="March"/>
-                <MenuItem value={4} primaryText="April"/>
-                <MenuItem value={5} primaryText="May"/>
-                <MenuItem value={6} primaryText="June"/>
-                <MenuItem value={7} primaryText="July"/>
-                <MenuItem value={8} primaryText="August"/>
-                <MenuItem value={9} primaryText="September"/>
-                <MenuItem value={10} primaryText="October"/>
-                <MenuItem value={11} primaryText="November"/>
-                <MenuItem value={12} primaryText="December"/>
+                <MenuItem value="January" primaryText="January"/>
+                <MenuItem value="February" primaryText="February"/>
+                <MenuItem value="March" primaryText="March"/>
+                <MenuItem value="April" primaryText="April"/>
+                <MenuItem value="May" primaryText="May"/>
+                <MenuItem value="June" primaryText="June"/>
+                <MenuItem value="July" primaryText="July"/>
+                <MenuItem value="August" primaryText="August"/>
+                <MenuItem value="September" primaryText="September"/>
+                <MenuItem value="October" primaryText="October"/>
+                <MenuItem value="November" primaryText="November"/>
+                <MenuItem value="December" primaryText="December"/>
               </SelectField>
           </span>
           <span style={styles.field}>
             <SelectField value={this.state.year} onChange={this.handleYearChange}>
-              <MenuItem value={1} primaryText="2014" />
-              <MenuItem value={2} primaryText="2015" />
+              <MenuItem value="2014" primaryText="2014" />
+              <MenuItem value="2015" primaryText="2015" />
             </SelectField>
           </span>
           <span style={styles.blankspace}></span>
